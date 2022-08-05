7cff482a921869c453924f14273f15fcdfce57c0
madden20202 <business.mocksock@gmail.com>
2022-08-05 17:05:47 -0500

    add route to db file

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -9,10 +10,10 @@ db = SQLAlchemy(app)
 # Schema Variable
 ma = Marshmallow(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 
 class Messages(db.Model):
-    
+
 
 if __name__ == "__main__":
     db.create_all()
