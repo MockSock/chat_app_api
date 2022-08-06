c8d0cadef9a290f625de195d6c2bc4b6826e2021
madden20202 <business.mocksock@gmail.com>
2022-08-05 21:11:39 -0500

    add all values for the table

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ ma = Marshmallow(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 
 class Messages(db.Model):
+    conversation_id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer)
+    # character limit is 500 
+    sender_name = db.Column(db.String(500))
+    content = db.Column(db.String(500))
+    time_sent = db.Column(db.String)
 
 
 if __name__ == "__main__":
