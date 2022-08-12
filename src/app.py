23e9062c5ca3c54a649e4d200a741aa6cdd280c4
madden20202 <business.mocksock@gmail.com>
2022-08-12 13:32:02 -0500

    move all files to an src folder

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import cross_origin, CORS
 
-from random_number import createRandomNumber
+from src.random_number import createRandomNumber
 
 import os
 
